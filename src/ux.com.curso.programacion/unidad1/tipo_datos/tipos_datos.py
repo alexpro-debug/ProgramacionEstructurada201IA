88af4263f44d5228c502d79a3e92c5e83d879aab
Alex <alexpro5098@gmail.com>
2026-03-10 08:36:56 -0600

    tipo_datos

---
+++ src/ux.com.curso.programacion/unidad1/tipo_datos/tipos_datos.py
@@ -0,0 +1,29 @@
+#demostracion
+
+def datos():
+    #demostracion de tipo de datos de python
+    entero =25
+    decimal =3.14
+    cadena ="we are zawarudo"
+    booleano =True
+
+    print(entero)
+    print(decimal)
+    print(cadena)
+    print(booleano)
+
+def tipos_datos_compuesto():
+    lista =[10,20,30,40,50]
+    tupla =(19,29,39,49,59)
+    diccionario = {"nombre": "Alex", "edad": 18, "ciudad": "xalapa"}
+
+    print(lista)
+    print(tupla)
+    print(diccionario)
+
+def main():
+    datos()
+    tipos_datos_compuesto()
+
+if __name__ == "__main__":
+    main()
