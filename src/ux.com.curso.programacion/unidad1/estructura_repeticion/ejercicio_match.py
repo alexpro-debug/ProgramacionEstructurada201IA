e3620ee11887c1ed357ec9f359b0bbbd5af1e0fa
Alex <alexpro5098@gmail.com>
2026-03-13 08:45:09 -0600

    ejercicios_cadenas_logica_match

---
+++ src/ux.com.curso.programacion/unidad1/estructura_repeticion/ejercicio_match.py
@@ -0,0 +1,27 @@
+#implementacin de match en python
+
+def demostracion():
+    print("-- Ejemplo de match")
+    opcion = input("ingrese una opcion (1-3): ")
+
+    match opcion:
+        case "1":
+            print("opcion 1 seleccionada")
+            nombre = input("ingrese su nombre: ")
+            print(f"hola, {nombre}")
+        case "2":
+            print("opcion 2 seleccionada")
+            matricula = input("ingrese su matricula: ")
+            print(f"su matricula es: {matricula}")
+        case "3":
+            print("opcion 3 seleccionada")
+            semestre = input("ingrese su semestre: ")
+            print(f"usted esta en el semetre: {semestre}")
+        case _:
+            print("opcion no valida")
+
+def main():
+    demostracion()
+
+if __name__ == "__main__":
+    main()
