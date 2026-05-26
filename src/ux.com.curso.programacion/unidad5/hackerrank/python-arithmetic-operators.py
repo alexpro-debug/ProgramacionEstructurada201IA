409e67eb594c488b71c70a750f27d8f252a68149
Alex <alexpro5098@gmail.com>
2026-05-26 08:12:38 -0600

    Elimine los textos pues no hacia falta ponerlos

--- src/ux.com.curso.programacion/unidad5/hackerrank/python-arithmetic-operators.py
+++ src/ux.com.curso.programacion/unidad5/hackerrank/python-arithmetic-operators.py
@@ -2,9 +2,6 @@ if __name__ == '__main__':
     a = int(input())
     b = int(input())
     
-    # 1. Suma
     print(a + b)
-    # 2. Resta (primero menos el segundo)
     print(a - b)
-    # 3. Producto
     print(a * b)
--- src/ux.com.curso.programacion/unidad5/hackerrank/python-division.py
+++ src/ux.com.curso.programacion/unidad5/hackerrank/python-division.py
@@ -2,7 +2,5 @@ if __name__ == '__main__':
     a = int(input())
     b = int(input())
     
-    # División entera
     print(a // b)
-    # División flotante (con decimales)
     print(a / b)
